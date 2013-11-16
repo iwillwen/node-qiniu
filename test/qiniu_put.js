bda5eb67b9909043829669aaac9a6b6b3c48d629
iwillwen <willwengunn@gmail.com>
2013-11-16 13:28:23 +0800

    Better CDN

--- test/qiniu_put.js
+++ test/qiniu_put.js
@@ -7,7 +7,7 @@ describe('qiniu.Put', function() {
   describe('Bucket.putFile()', function() {
     it('should uploads a file with giving a pathname', function(done) {
       
-      qiniu.testBucket.putFile('gogopher.jpg', __dirname + '/assets/gogopher.jpg', function(err) {
+      qiniu.testBucket.putFile('gogopher.jpg', __dirname + '/assets/gogopher.jpg', function(err, reply) {
         if (err) {
           throw err;
         }
